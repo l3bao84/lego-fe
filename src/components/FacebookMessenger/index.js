22be65efd1fd9533e0a3b51ca6881f975be6637c
l3bao84 <l3ducb4008@gmail.com>
2024-05-19 02:14:27 +0700

    fix

--- src/components/FacebookMessenger/index.js
+++ src/components/FacebookMessenger/index.js
@@ -2,8 +2,8 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 function FaceBookMsg() {
     return (
-        <FacebookProvider appId="937785827910171" chatSupport>
-            <CustomChat pageId="61559712751649" minimized="true" />
+        <FacebookProvider appId="428853872884994" chatSupport>
+            <CustomChat pageId="309005125632399" minimized="true" />
         </FacebookProvider>
     );
 }
