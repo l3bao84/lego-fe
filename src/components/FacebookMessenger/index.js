e531180ec6255a8bd12c47c287e5c65c8f23e90e
l3bao84 <l3ducb4008@gmail.com>
2024-05-19 01:56:47 +0700

    fix page id

--- src/components/FacebookMessenger/index.js
+++ src/components/FacebookMessenger/index.js
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 function FaceBookMsg() {
     return (
       <FacebookProvider appId="937785827910171" chatSupport>
-        <CustomChat pageId="309005125632399" minimized="true"/>
+        <CustomChat pageId="61559712751649" minimized="true"/>
       </FacebookProvider>    
     )
 }
