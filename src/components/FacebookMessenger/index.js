9f655fc20bd62123216e9a7a39feb7f13e671f76
l3bao84 <l3ducb4008@gmail.com>
2024-05-19 02:08:41 +0700

    fix CSP

--- src/components/FacebookMessenger/index.js
+++ src/components/FacebookMessenger/index.js
@@ -2,10 +2,10 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 function FaceBookMsg() {
     return (
-      <FacebookProvider appId="937785827910171" chatSupport>
-        <CustomChat pageId="61559712751649" minimized="true"/>
-      </FacebookProvider>    
-    )
+        <FacebookProvider appId="937785827910171" chatSupport>
+            <CustomChat pageId="61559712751649" minimized="true" />
+        </FacebookProvider>
+    );
 }
 
 export default FaceBookMsg;
