d452a63f8a0b1c20f16d03fd0cfd8b53c6255a03
l3bao84 <l3ducb4008@gmail.com>
2024-05-19 14:02:30 +0700

    fix fb chat

--- src/components/FacebookMessenger/index.js
+++ src/components/FacebookMessenger/index.js
@@ -1,8 +1,28 @@
 import { FacebookProvider, CustomChat } from 'react-facebook';
+import { useEffect } from 'react';
 
 function FaceBookMsg() {
+    useEffect(() => {
+        window.fbAsyncInit = function () {
+            FB.init({
+                xfbml: true,
+                version: 'v12.0',
+            });
+        };
+
+        (function (d, s, id) {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+    }, []);
+
     return (
-        <FacebookProvider appId="937785827910171" chatSupport>
+        <FacebookProvider appId="937785827910171" chatSupport xfbml>
             <CustomChat pageId="309005125632399" minimized="true" />
         </FacebookProvider>
     );
